0115d55e83ff110664ea4847d9d79938ea6d5352
Riotpiaole <locartrock@gmail.com>
2018-03-06 17:14:02 -0500

    Add template

---
+++ server/route.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os 
+
+template_dir = os.path.abspath( "../template/" )
+
+app = Flask( "Server" , template_folder = template_dir )
+
+from flask import render_template
+
+# Adding routes over here 
+
+@app.route( "/" )
+@app.route( "/main" )
+def main():
+    user = {'username': 'Miguel'}
+    return render_template( 'index.html' 
+        , user=user )
